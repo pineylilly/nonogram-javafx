a82d0fd410e11077020d2bc915775d1716edb135
Werapat <hellp002@gmail.com>
2024-01-11 11:58:48 +0700

    feat: init cell button

---
+++ src/gui/CellPane.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javafx.scene.layout.Pane;
+
+public class CellPane extends Pane {
+    private final int width = 0;
+    private final int height =0;
+
+    public CellPane(){
+        super();
+
+    }
+
+    private void handleOnClick(){
+
+    }
+
+
+}
